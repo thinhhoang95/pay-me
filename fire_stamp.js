d97ea84720a717430ee80565b68908f85bdd256e
Thinh Hoang <thinhhoangdinh95@hotmail.com>
2021-05-23 20:31:48 +0200

    Add the ability to claim units

---
+++ fire_stamp.js
@@ -0,0 +1,48 @@
+const moment = require("moment");
+const admin = require("firebase-admin");
+const serviceAccount = require("./payme-node-key.json");
+const reader = require("readline-sync"); //npm install readline-sync
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+var fs = require("fs");
+
+const make_serial = (length, terms) => {
+  var result = [];
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var charactersLength = characters.length;
+  for (let term = 0; term < terms; term++) {
+    for (var i = 0; i < length; i++) {
+      result.push(
+        characters.charAt(Math.floor(Math.random() * charactersLength))
+      );
+    }
+    if (term != terms - 1) result.push("-");
+  }
+  return result.join("");
+};
+
+const preprocess = async () => {
+  return new Promise(async (resolve, reject) => {
+    // Prompt for the number of timeunits
+    let timeunits = reader.question("Number of time units accumulated: ");
+    let stampValue = Number(timeunits) * 1.8;
+    console.log('The stamp value is: ' + stampValue.toFixed(2))
+    // Add this stamp to the database for claim from the app
+    let sn = make_serial(5,5)
+    const stampRef = db.collection("salary").doc(sn);
+    stampRef.set({
+      'sn': sn,
+      'content': 'Salary for ' + timeunits + ' time units, available from ' + moment().toISOString() + '.',
+      'claimed': false,
+      'value': stampValue
+    });
+    resolve();
+  });
+};
+
+preprocess();
